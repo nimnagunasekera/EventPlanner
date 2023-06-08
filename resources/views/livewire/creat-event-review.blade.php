cf74c78673380b6c44e7d4462e4acdc09f66cc56
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 17:00:48 +0530

    Update creat-event-review.blade.php

--- resources/views/livewire/creat-event-review.blade.php
+++ resources/views/livewire/creat-event-review.blade.php
@@ -28,7 +28,7 @@
                 @enderror
 
             <label for="comment" class="sr-only">Comment</label>
-            <textarea rows="6" name="comment" id="comment" wire:model="comment"
+            <textarea rows="3" name="comment" id="comment" wire:model="comment"
                 class="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                 placeholder="Write about your experince..."></textarea>
                 @error('comment')
