3b141c8b0a55975f877a68ff4b924d6fc74290bc
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 16:35:24 +0530

    Update creat-event-review.blade.php

--- resources/views/livewire/creat-event-review.blade.php
+++ resources/views/livewire/creat-event-review.blade.php
@@ -1,17 +1,26 @@
 <div class="pt-5">
     <h3 class="text-lg font-medium leading-7 text-gray-900">Post A Review</h3>
-    <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Write about your experince during your stay</p>
     <form action="#" class="relative bg-white pt-4">
+
+        <select name="rating" id="rating" class="mb-3 rounded border-gray-300">
+            <option value="" selected disabled>Rating</option>
+            <option value="1">1 - Poor</option>
+            <option value="2">2 - Fair</option>
+            <option value="3">3 - Good</option>
+            <option value="4">4 - Very Good</option>
+            <option value="5">5 - Excellent</option>
+        </select>
+
         <div
             class="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
             <label for="title" class="sr-only">Title</label>
             <input type="text" name="title" id="title"
                 class="block w-full border-0 pt-2.5 text-lg font-medium placeholder:text-gray-400 focus:ring-0"
-                placeholder="Title">
-            <label for="description" class="sr-only">Description</label>
-            <textarea rows="6" name="description" id="description"
+                placeholder="Review Title">
+            <label for="comment" class="sr-only">Comment</label>
+            <textarea rows="6" name="comment" id="comment"
                 class="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                placeholder="Write a description..."></textarea>
+                placeholder="Write about your experince..."></textarea>
 
             <!-- Spacer element to match the height of the toolbar -->
             <div aria-hidden="true">
