ddcd89037560b13f9dffc167b1cc1b645a1903a2
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 18:01:58 +0530

    implemented the logic for star rating

--- resources/views/livewire/event-reviews.blade.php
+++ resources/views/livewire/event-reviews.blade.php
@@ -6,8 +6,28 @@
                 <div class="lg:col-span-8 lg:col-start-5 xl:col-span-9 xl:col-start-4 xl:grid xl:grid-cols-3 xl:items-start xl:gap-x-8">
                     <div class="flex items-center xl:col-span-1">
                         <div class="flex items-center">
+
+                            @foreach (range(1, $review->rating) as $rating)
+                                <svg class="text-yellow-400 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            @endforeach
+                            @if ($review->rating < 5)
+                                @foreach (range(1, (5 - $review->rating)) as $rating)
+                                    <svg class="text-gray-200 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20"
+                                        fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                            d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                @endforeach
+                            @endif
+
                             <!-- Active: "text-yellow-400", Inactive: "text-gray-200" -->
-                            <svg class="text-yellow-400 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20"
+                            {{-- <svg class="text-yellow-400 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20"
                                 fill="currentColor" aria-hidden="true">
                                 <path fill-rule="evenodd"
                                     d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
@@ -36,9 +56,10 @@
                                 <path fill-rule="evenodd"
                                     d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
                                     clip-rule="evenodd" />
-                            </svg>
+                            </svg> --}}
                         </div>
-                        <p class="ml-3 text-sm text-gray-700">5
+                        <p class="ml-3 text-sm text-gray-700">
+                            <span >{{ $review->rating }}</span>
                             <span > out of 5 stars</span>
                         </p>
                     </div>
