2f476c009220eea3476fa3ec19a3592a501dcf3c
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 20:43:43 +0530

    added reservation model into livewire component
    added reservation model into livewire component to start binding the data.
    
    used mount method to mount it to the event.
    
    added user as a public variable to take the authenticated user into that variable so that the user will have his own details.
    
    defined public reservation variable to have a reservation object saved to start filling the object with details.

--- app/Http/Livewire/EventReservationForm.php
+++ app/Http/Livewire/EventReservationForm.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Reservation;
 use Livewire\Component;
 
 class EventReservationForm extends Component
 {
+    public $event;
+
+    public $user;
+
+    public Reservation $reservation;
+
+    public function mount($event)
+    {
+        $this->event = $event;
+        $this->user = auth()->user();
+    }
+
     public function render()
     {
         return view('livewire.event-reservation-form');
