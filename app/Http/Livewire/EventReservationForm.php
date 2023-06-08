83530ef8c128a6c8a4f26c79a1dd34420be8292b
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 20:16:02 +0530

    created livewire component for event reservation form
    created livewire component  and moved the object to the livewire blade

---
+++ app/Http/Livewire/EventReservationForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EventReservationForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.event-reservation-form');
+    }
+}
