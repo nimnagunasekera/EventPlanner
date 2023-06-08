d82756e2872692143430313bd2ff4d1b19d190f1
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 15:54:19 +0530

    created another livewire component
    created another liver wire component to pass in the "add a review" portion of the review part

---
+++ app/Http/Livewire/CreatEventReview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CreatEventReview extends Component
+{
+    public function render()
+    {
+        return view('livewire.creat-event-review');
+    }
+}
