e94ad695de749fd89daa87199b805d6f76866cca
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 20:23:54 +0530

    plugged the reservation file

--- resources/views/event/reservation.blade.php
+++ resources/views/event/reservation.blade.php
@@ -1,18 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Reserve Event X') }}
+            Register for {{ $event->event_name }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-            @livewire('event.reservation-form', [
-                'event' => $event
+            @livewire('event-reservation-form', [
+                'event' => $event,
             ])
-
-
         </div>
 
     </div>
