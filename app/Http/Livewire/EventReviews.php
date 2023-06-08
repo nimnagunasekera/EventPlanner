f02638c552bd09698610ba6919f3a44e833a8839
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 19:24:40 +0530

    binded refresh action to reviewAdded event
    binded refresh action to reviewAdded event when it is emited in the save function of the CreateEventReview livewire component.

--- app/Http/Livewire/EventReviews.php
+++ app/Http/Livewire/EventReviews.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Event;
 use Livewire\Component;
 
 class EventReviews extends Component
 {
     public $event;
+
+    // refresh the component when a new review is added
+    protected $listeners = [
+        'reviewAdded' => '$refresh'
+    ];
 
     public function mount($event)
     {
@@ -15,6 +21,8 @@ class EventReviews extends Component
 
     public function render()
     {
-        return view('livewire.event-reviews');
+        return view('livewire.event-reviews', [
+            'reviews' => $this->event->reviews()->latest()->get()
+        ]);
     }
 }
