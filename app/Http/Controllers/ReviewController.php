7bf56cb7948c52caed6456921a12771dd3ac0e86
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-06-08 16:13:23 +0530

    fixed error
    switched up the review controller and the review model

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,31 +6,5 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    use HasFactory;
-
-    protected $attributes = [
-        'rating' => 1
-    ];
-
-    protected $fillable = [
-        'event_id',
-        'user_id',
-        'rating',
-        'title',
-        'comment',
-    ];
-
-    protected $casts = [
-        'rating' => 'integer',
-    ];
-
-    public function event()
-    {
-        return $this->belongsTo(Event::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 }
